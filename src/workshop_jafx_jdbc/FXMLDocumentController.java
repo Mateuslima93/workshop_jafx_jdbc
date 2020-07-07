a341d080fe5e75fe272a1053088828f84814848b
Mateuslima93 <mateus_limaarodrigues@hotmail.com>
2020-07-07 15:38:26 -0300

    Main View Controller

--- src/workshop_jafx_jdbc/FXMLDocumentController.java
+++ src/workshop_jafx_jdbc/FXMLDocumentController.java
@@ -11,20 +11,31 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 
 /**
  *
  * @author MASTER
  */
 public class FXMLDocumentController implements Initializable {
-    
     @FXML
-    private Label label;
-    
+    private MenuItem menuItemSeller;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
+    
+    @FXML 
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        System.out.println("onMenuItemDepartmentAction");
+    }
+    @FXML
+    public void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
     }
     
     @Override
