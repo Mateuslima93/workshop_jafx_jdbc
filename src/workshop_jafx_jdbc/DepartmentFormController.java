954412dad1912e15eed5598f34f2a724c35c7931
Mateuslima93 <mateus_limaarodrigues@hotmail.com>
2020-07-09 16:19:36 -0300

    DepartmentForm Controller

---
+++ src/workshop_jafx_jdbc/DepartmentFormController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop_jafx_jdbc;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import workshop.util.Constraints;
+
+/**
+ *
+ * @author MASTER
+ */
+public class DepartmentFormController implements Initializable {
+    @FXML
+    private TextField txtId;
+    
+    @FXML
+    private TextField txtName;
+    
+    @FXML
+    private Label labelErrorName;
+    
+    @FXML
+    private Button btSave;
+    
+    @FXML
+    private Button btCancel;
+    
+    @FXML
+    public void onBtSaveAction(){
+        System.out.println("onBtSaveAction");
+    }
+    @FXML
+    public void onBtCancelAction(){
+        System.out.println("onBtCancelAction");
+    }
+    
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeNodes();
+    }
+ 
+    private void initializeNodes(){
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+}
